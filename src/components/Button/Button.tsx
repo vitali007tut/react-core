7b8a8e54598d9b080b252ee7ff81baa9b538ed14
Vitali <vitali007@tut.by>
2024-03-03 15:37:55 +0300

    choers

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+import { FC } from 'react';
+import { Button as ButtonMUI } from '@mui/material';
+import type { ButtonProps as ButtonMUIProps } from '@mui/material';
+
+export const Button: FC<ButtonMUIProps> = ({ children, size, endIcon, type = 'button' }) => {
+    return (
+        <ButtonMUI type={type} variant='contained' color='error' size={size} endIcon={endIcon}>
+            {children}
+        </ButtonMUI>
+    );
+};
---
+++ src/components/Navigation/Logined/Logined.tsx
@@ -0,0 +1,10 @@
+// import Button from '../../Button/Button.tsx'
+// import React from 'react';
+
+function Logined(/*props*/) {
+    return (
+        <div></div>
+    );
+}
+
+export default Logined;

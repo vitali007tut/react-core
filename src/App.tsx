9eca9a4fa3e82e8e16eee8737eb8a8d0cf84f510
Vitali <vitali007@tut.by>
2024-03-21 16:24:48 +0300

    feat: add lazy suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Suspense, lazy } from "react";
 import s from "./App.module.css";
-import Header from "./components/Header/Header.tsx";
-import LoginPage from "./pages/LoginPage/LoginPage.tsx";
-import SignUpPage from "./pages/SignUpPage/SignUpPage.tsx";
-import MainPage from "./pages/MainPage/MainPage.tsx";
-import HistoryPage from "./pages/HistoryPage/HistoryPage.tsx";
-import FavoritesPage from "./pages/FavoritesPage/FavoritesPage.tsx";
-import DetailsPage from "./pages/DetailsPage/DetailsPage.tsx";
+import Header from "./components/Header/Header";
+import MainPage from "./pages/MainPage/MainPage";
+const DetailsPage = lazy(() => import('./pages/DetailsPage/DetailsPage'))
+const FavoritesPage = lazy(() => import("./pages/FavoritesPage/FavoritesPage"));
+const HistoryPage = lazy(() => import("./pages/HistoryPage/HistoryPage"));
+const LoginPage = lazy(() => import("./pages/LoginPage/LoginPage"));
+const SignUpPage = lazy(() => import("./pages/SignUpPage/SignUpPage"));
 import { ErrorBoundary } from "react-error-boundary";
 
 function App() {
@@ -15,14 +16,16 @@ function App() {
             <BrowserRouter>
                 <div className={s.container}>
                     <Header />
-                    <Routes>
-                        <Route path="/" element={<MainPage />} />
-                        <Route path="login" element={<LoginPage />} />
-                        <Route path="signup" element={<SignUpPage />} />
-                        <Route path="favorites" element={<FavoritesPage />} />
-                        <Route path="history" element={<HistoryPage />} />
-                        <Route path="details/:id" element={<DetailsPage />} />
-                    </Routes>
+                    <Suspense fallback={<div>Loading....</div>}>
+                        <Routes>
+                            <Route path="/" element={<MainPage />} />
+                            <Route path="login" element={<LoginPage />} />
+                            <Route path="signup" element={<SignUpPage />} />
+                            <Route path="favorites" element={<FavoritesPage />} />
+                            <Route path="history" element={<HistoryPage />} />
+                            <Route path="details/:id" element={<DetailsPage />} />
+                        </Routes>
+                    </Suspense>
                 </div>
             </BrowserRouter>
         </ErrorBoundary>
